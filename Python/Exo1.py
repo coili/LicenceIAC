7ad3544de27c1094463f25cf86a53c41f769bcec
Colin Senot <contact@colin-senot.fr>
2021-10-25 16:59:36 +0200

    Exercice 1 : terminé

--- Python/Exo1.py
+++ Python/Exo1.py
@@ -11,28 +11,28 @@ class CompteBancaire:
     @param solde : solde du compte
     """
     def __init__(self, numero, nom, solde):
-        self.numero = numero
-        self.nom = nom
-        self.solde = solde
+        self.__numero = numero
+        self.__nom = nom
+        self.__solde = solde
 
     """
     Méthode credit : ajoute une somme d'argent au solde
     @param somme : somme d'argent à ajouter
     """
     def credit(self, somme):
-        self.solde += somme
+        self.__solde += somme
 
     """
     Méthode debit : retire une somme d'argent au solde
     @param somme : somme d'argent à retirer
     """
     def debit(self, somme):
-        self.solde -= somme
+        self.__solde -= somme
 
     """
     Méthode afficher : affiche les détails du compte
     """
     def afficher(self):
-        print("Compte numéro : ", self.numero)
-        print("Propriétaire : ", self.nom)
-        print("Solde du compte : ", self.solde, " €")
+        print("Compte numéro : ", self.__numero)
+        print("Propriétaire : ", self.__nom)
+        print("Solde du compte : ", self.__solde, " €")
