5895b3cf39891bd450358f6ec62aee51bd82d657
Colin Senot <colin.senot21@gmail.com>
2021-10-26 08:24:00 +0200

    Mise à jour

--- Python/temp.py
+++ Python/temp.py
@@ -1,6 +1,32 @@
-largeurRectangle = 20
-longRectangle = 30
+class Rectangle:
 
-surface = largeurRectangle * longRectangle
+    def __init__(self, longueur, largeur):
+        self.__longueur = longueur
+        self.__largeur = largeur
 
-print(surface)
+
+    """
+    GETTER & SETTER
+    """
+    def getLongueur(self):
+        return self.__longueur
+    
+    def getLargeur(self):
+        return self.__largeur
+
+    def setLongueur(self, longueur):
+        self.__longueur = longueur
+    
+    def setLargeur(self, largeur):
+        self.__largeur = largeur
+
+    
+    longueur = property(getLongueur, setLongueur)
+    largeur = property(getLargeur, setLargeur)
+    
+
+""""
+TEST
+"""    
+rectangle_1 = Rectangle(20, 30)
+rectangle_2 = Rectangle(20, 20)
