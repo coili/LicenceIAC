871471c4df3b9b1b17a5818ce63405c2cfcf4292
Colin Senot <colin.senot21@gmail.com>
2021-12-08 11:26:11 +0100

    Exercice 2 en haskell

---
+++ Haskell/Cours 1/Exercice 2.hs
@@ -0,0 +1,16 @@
+f1 x = a*x*x + b*x + c 
+    where a = 6
+          b = 42
+          c = 78
+
+f2 x = let a = 6 
+           b = 42
+           c = 78
+         in a*x*x + b*x + c   
+
+x1 = f1 53
+x2 = f2 53
+
+main = do
+    print x1
+    print x2
